fa5c40c08cc411392c19d10ee3ae1ffcf1b88d47
Neville Ryant <nryant@gmail.com>
2017-07-28 11:59:46 -0400

    Move SoX manipulations to convert_to_wav in seglib.utils.

--- seglib/utils.py
+++ seglib/utils.py
@@ -1,15 +1,123 @@
 # Copyright (c) 2012-2017, Trustees of the University of Pennsylvania
 # Authors: nryant@ldc.upenn.edu (Neville Ryant)
 # License: BSD 2-clause
-"""TODO"""
+"""Miscellaneous utility functions related to audio and segmentation."""
+from __future__ import absolute_import
+from __future__ import print_function
 from __future__ import unicode_literals
+import os
+import subprocess
 
-__all__ = ['elim_short_segs', 'merge_segs']
+__all__ = ['concat_segs', 'convert_to_wav', 'elim_short_segs', 'get_dur',
+           'merge_segs']
+
+
+def get_dur(af):
+    """Return duration of audio file.
+
+    Parameters
+    ----------
+    af : str
+        Path to audio file. May be in any format understood by SoX.
+
+    Returns
+    -------
+    dur : float
+        Duration of audio file (seconds).
+    """
+    try:
+        cmd = ['soxi', '-D', af]
+        with open(os.devnull, 'wb') as f:
+            dur = float(subprocess.check_output(cmd, stderr=f))
+    except subprocess.CalledProcessError:
+        raise IOError('Error opening: %s' % af)
+    return dur
+
+
+def convert_to_wav(wf, af, channel=1, start=0, end=None):
+    """Convert audio file to 16 kHz, 16 bit WAV file.
+
+    Parameters
+    ----------
+    wf : str
+       Output WAV file.
+
+    af : str
+        Path to audio file containing the recording. May be in any format
+        understood by SoX.
+
+    target_sr : int, optional
+        Target sample rate (Hz), to which recording will be resampled using
+        SoX. If None, do not resample.
+        (Default: Recording sample rate.)
+
+    channel : int, optional
+        Channel to extract (1-indexed).
+        (Default: 1)
+
+    start : float, optional
+        Time (seconds) of center of first frame.
+        (Default: 0)
+
+    end : float, optional
+        Time (seconds) of center of last frame.
+        (Default: Duration of recording.)
+    """
+    if end is None:
+        end = get_dur(af)
+
+    # Resample and load into array.
+    try:
+        cmd = ['sox', af,
+               '-b', '16', # Make 16-bit
+               '-e', 'signed-integer', # And linear PCM
+               '-t', 'wav',
+               wf,
+               'remix', str(channel), # Extract single channel.
+               'trim', str(start), '=%s' % end,
+               'rate', '16000',
+               ]
+        with open(os.devnull, 'wb') as f:
+            raw = subprocess.check_output(cmd, stderr=f)
+    except subprocess.CalledProcessError:
+        raise IOError('Error opening: %s' % af)
+
+
+def concat_segs(seg_seqs, dur=None):
+    """Concatenate multiple segmentations.
+
+    Parameters
+    ----------
+    seg_seqs : list of list
+        List of segmentations.
+
+    dur : float, optional
+        Duration of the derived segmentation. If None, will be determined
+        automatically.
+        (Default: None)
+
+    Returns
+    -------
+    segs : list of tuple
+        List of segments, each expressed as a tuple
+        (``onset``, ``offset``, ``label``), where ``onset`` and ``offset`` are
+        the onset and offset of the segment in seconds relative to the start
+        of the recording.
+    """
+    rec_onset = 0.0
+    segs = []
+    for segs_ in seg_seqs:
+        for onset, offset, label in segs_:
+            segs.append([onset + rec_onset, offset + rec_onset, label])
+        rec_onset = segs[-1][1]
+    if dur is not None:
+        if dur > segs[-1][1]:
+            segs[-1][1] = dur
+    return segs
 
 
 def merge_segs(segs):
-    """Merge sequences of segments with same label.
-    """
+    """Merge sequences of segments with same label."""
     new_segs = []
     while len(segs) > 1:
         curr = segs.pop()
