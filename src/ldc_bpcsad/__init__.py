d02538ec8710c5532db5ae06aff2dfe854c16f7f
nryant <nryant@gmail.com>
2022-11-16 11:33:24 -0500

    license: update license to GPL v3

--- src/ldc_bpcsad/__init__.py
+++ src/ldc_bpcsad/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2012-2022, Trustees of the University of Pennsylvania
 # Authors: nryant@ldc.upenn.edu (Neville Ryant)
-# License: BSD 2-clause
+# License: GNU General Public License v3.0
 from . import _version
 __version__ = _version.get_versions()['version']
--- src/ldc_bpcsad/io/__init__.py
+++ src/ldc_bpcsad/io/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2012-2022, Trustees of the University of Pennsylvania
 # Authors: nryant@ldc.upenn.edu (Neville Ryant)
-# License: BSD 2-clause
+# License: GNU General Public License v3.0
 """Functions for reading/writing various segmentation file formats."""
 from .audacity import load_audacity_label_file, write_audacity_label_file
 from .htk import load_htk_label_file, write_htk_label_file
