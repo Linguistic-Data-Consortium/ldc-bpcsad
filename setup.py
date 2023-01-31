afdc935c20f4feb4d826a249cc6c50a1cac32ae3
Neville Ryant <nryant@gmail.com>
2023-01-31 17:34:27 -0500

    refactor: update LICENSE preambles

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-# Copyright (c) 2012-2022, Trustees of the University of Pennsylvania
-# Authors: nryant@ldc.upenn.edu (Neville Ryant)
-# License: GNU General Public License v3.0
+# Copyright (c) 2023, Trustees of the University of Pennsylvania
+# See LICENSE for licensing conditions
 from setuptools import setup, find_packages
 
 import versioneer
